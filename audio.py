65d52394ad3b537d9f917ba6a0d6623a370bf838
wxywb <xy.wang@zilliz.com>
2023-04-20 16:52:04 +0800

    init project.
    Signed-off-by: wxywb <xy.wang@zilliz.com>

---
+++ audio.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from PIL import Image
+import os
+import io
+import base64
+from io import BytesIO
+import requests
+
+from gptcache import cache
+from gptcache.manager import get_data_manager, CacheBase, VectorBase, ObjectBase
+from gptcache.adapter import openai
+from gptcache.processor.pre import get_file_name
+from gptcache.embedding import Data2VecAudio
+from gptcache.similarity_evaluation.distance import SearchDistanceEvaluation
+
+
+@st.cache_resource
+def initialize_configuration():
+    data2vec = Data2VecAudio()
+    data_manager = get_data_manager(CacheBase('sqlite', sql_url='sqlite:///./local/gptcache20.db'),
+                                    VectorBase('faiss', dimension=data2vec.dimension, index_path='./local/faiss20.index'),
+                                    ObjectBase('local', path='./local'))
+    cache.init(
+        pre_embedding_func=get_file_name,
+        embedding_func=data2vec.to_embeddings,
+        data_manager=data_manager,
+        similarity_evaluation=SearchDistanceEvaluation(),
+        )
+    return data2vec, data_manager
+
+data2vec, data_manager = initialize_configuration()
+
+def api_call(audio_bytes, open_ai_key):
+    os.environ['OPENAI_API_KEY'] =  open_ai_key
+    os.environ['CURL_CA_BUNDLE'] = ''
+    os.environ['KMP_DUPLICATE_LIB_OK']='TRUE'
+    cache.set_openai_key()
+    transcript = openai.Audio.transcribe("whisper-1", audio_bytes)
+    is_cached = transcript.get("gptcache", False)
+    return transcript['text'], is_cached
+    
+def main():
+
+    st.title("GPTCache for Audio Demo")
+    open_ai_key = st.text_input("OpenAI key")
+    audio_file = st.file_uploader("Choose an audio file (.mp3, .wav, or .ogg)", type=["mp3", "wav", "ogg"])
+    if st.button('generate', key='button1'):
+        file_path = os.path.abspath(audio_file.name)
+        audio_file_handler = open(file_path, "rb")
+        text, is_cached = api_call(audio_file_handler, open_ai_key)
+        st.write(text)
+        if is_cached:
+                st.markdown('<div style="display: flex; align-items: center; justify-content: center; background-color: green; padding: 10px; color: white; font-weight: bold; border-radius: 5px; margin: 10px auto; max-width: 100px;">cache</div>', unsafe_allow_html=True)
+   
+if __name__ == '__main__':
+    main()
