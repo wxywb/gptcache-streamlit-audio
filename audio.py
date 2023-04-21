ecb533c9007be72cee57252f7049c98fb5d999c5
wxywb <xy.wang@zilliz.com>
2023-04-21 11:51:39 +0800

    Save the uploaded file as a temp file.
    Signed-off-by: wxywb <xy.wang@zilliz.com>

--- audio.py
+++ audio.py
@@ -5,6 +5,9 @@ import io
 import base64
 from io import BytesIO
 import requests
+import uuid
+import torchaudio
+import hashlib
 
 from gptcache import cache
 from gptcache.manager import get_data_manager, CacheBase, VectorBase, ObjectBase
@@ -44,13 +47,35 @@ def main():
     st.title("GPTCache for Audio Demo")
     open_ai_key = st.text_input("OpenAI key")
     audio_file = st.file_uploader("Choose an audio file (.mp3, .wav, or .ogg)", type=["mp3", "wav", "ogg"])
+
     if st.button('generate', key='button1'):
-        file_path = os.path.abspath(audio_file.name)
-        audio_file_handler = open(file_path, "rb")
-        text, is_cached = api_call(audio_file_handler, open_ai_key)
-        st.write(text)
-        if is_cached:
-                st.markdown('<div style="display: flex; align-items: center; justify-content: center; background-color: green; padding: 10px; color: white; font-weight: bold; border-radius: 5px; margin: 10px auto; max-width: 100px;">cache</div>', unsafe_allow_html=True)
+        file_extension = os.path.splitext(audio_file.name)[1]  # Get the extension of the uploaded file
+        random_filename = str(uuid.uuid4()) + file_extension
+        with open(random_filename, 'wb') as f:
+            f.write(audio_file.getbuffer())
+        print(random_filename)
+        print('os.listdir())', os.listdir()) 
+        print('hello') 
+        audio_file_handler = open(random_filename, 'rb') 
+        exist = os.path.exists(random_filename)
+        file_content = audio_file_handler.read() 
+
+        info = torchaudio.info(random_filename)
+
+        # Combine the information into a single string
+        info_str = 'Sample rate: {}, Number of channels: {}, Number of samples: {}, Duration (seconds): {}'.format(info.sample_rate, info.num_channels, info.num_frames, info.num_frames / info.sample_rate)
+
+        print(info_str)
+        md5_hash = hashlib.md5(file_content).hexdigest()
+        st.write(info_str)
+        #torchaudio.load(random_filename)
+
+       # text, is_cached = api_call(audio_file_handler, open_ai_key)
+       # st.write(text)
+       # os.remove(random_filename)
+       # 
+       # if is_cached:
+       #         st.markdown('<div style="display: flex; align-items: center; justify-content: center; background-color: green; padding: 10px; color: white; font-weight: bold; border-radius: 5px; margin: 10px auto; max-width: 100px;">cache</div>', unsafe_allow_html=True)
    
 if __name__ == '__main__':
     main()
